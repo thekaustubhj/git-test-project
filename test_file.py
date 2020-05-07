e7be065b8e2edc6db45b259a5a153ecabea797fd
thekaustubhj <kaustubh.j.85@gmail.com>
2020-05-07 14:08:54 +1200

    Added imports for all key Pythn libraries

---
+++ test_file.py
@@ -0,0 +1,25 @@
+import re
+import matplotlib as plt
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.SVM import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.cluster import KMeans
+import requests
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.util import ngrams
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk import RegexpParser
